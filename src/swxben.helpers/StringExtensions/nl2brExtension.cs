0bf6f2f36aa4718aa727585cf70ea7cea28b0bcb
Ben Scott <ben@belfryimages.com.au>
2013-01-31 02:12:15 +1000

    fix nl2br

--- src/swxben.helpers/StringExtensions/nl2brExtension.cs
+++ src/swxben.helpers/StringExtensions/nl2brExtension.cs
@@ -4,7 +4,7 @@ namespace swxben.helpers.StringExtensions
 {
     public static class nl2brExtension
     {
-        public static string nl2br(string value)
+        public static string nl2br(this string value)
         {
             return value.Replace(Environment.NewLine, "<br/>");
         }
